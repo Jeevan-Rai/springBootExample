ad8e3fa299c52cc381e1e36039f4d5577bbff4e7
Jeevan-Rai <78399976+Jeevan-Rai@users.noreply.github.com>
2023-11-13 11:34:13 +0530

    Initial commit

---
+++ src/main/java/com/appu/springBootExample/MyRestController.java
@@ -0,0 +1,7 @@
+package com.appu.springBootExample;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+}
---
+++ src/main/java/com/appu/springBootExample/MyService.java
@@ -0,0 +1,7 @@
+package com.appu.springBootExample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+}
